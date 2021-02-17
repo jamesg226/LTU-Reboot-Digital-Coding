f08c0280b3278cb76212899cadb3d8436eb8073c
jamesg226 <77463925+jamesg226@users.noreply.github.com>
2021-02-17 19:25:09 +0000

    Complete task 3.2.3

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -10,5 +10,15 @@ var message = greet('Hello', 'Luke');
 /* EXAMPLE */
 
 // ADD YOUR CODE BELOW HERE
+function fullName(firstName, lastName) {
+  return firstName + ' ' + lastName ;
+  }
+  
+  console.log( fullName('Elijah', 'Wolffe'));
+  console.log( fullName('Mia', 'Wolffe'));
+  
 
+  var person1 = fullName('Amy', 'Dubunowski');
+  console.log( 'person1 variable: ' + person1)
+  console.log (person1)
 // ADD YOUR CODE ABOVE HERE
