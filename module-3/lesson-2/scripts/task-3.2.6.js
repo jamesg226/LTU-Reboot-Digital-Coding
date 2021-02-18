80d51d35bc78c6632f2646bfb910d7eb174991d5
jamesg226 <77463925+jamesg226@users.noreply.github.com>
2021-02-18 15:20:12 +0000

    Update task-3.2.6.js

--- module-3/lesson-2/scripts/task-3.2.6.js
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -1,12 +1,15 @@
 //Shall I wear a coat? with a logical operator (if temp is less than 30 or more than 5)
-var temperature = 6;
+var temperature = 49;
+var season = 'summer';
 
 if ( temperature < 0 ) {
     console.log( 'The temperature is ' + temperature + ', so you should stay inside!');
- } else if ( temperature < 30 || temperature > 5) {
+ } else if ( temperature < 30) {
     console.log( 'The temperature is ' + temperature + ', so you should wear a coat and a hat!');
-} else if ( temperature < 50 ) {
-console.log( 'The temperature is ' + temperature + ', so you should wear a coat!');
+} else if ( temperature < 50 && season === 'summer' ) {
+console.log( 'The temperature is ' + temperature + ', and it is summer so go outside!');
 } else {
      console.log( 'Shorts and a T-shirt are just fine!');
-    }
+}
+
+console.log (temperature);
