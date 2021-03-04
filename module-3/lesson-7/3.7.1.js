7a90864adc789c50911e8ecc2d150e53c0613ece
jamesg226 <77463925+jamesg226@users.noreply.github.com>
2021-03-04 21:06:29 +0000

    Complete task 3.7.1

--- module-3/lesson-7/3.7.1.js
+++ module-3/lesson-7/3.7.1.js
@@ -1,1 +1,39 @@
-console.log('James is awesome.');
+//console.log('James is awesome.');
+
+/*-----     Variables     ------*/
+const arrAnimalFilters = document.querySelectorAll('.animalFilter');
+const arrAnimalImages = document.querySelectorAll('.animalImage');
+//console.log(arrAnimalFilters);
+console.log(arrAnimalImages);
+
+
+/*-----     Event Listeners     ------*/
+for(counter=0;counter < arrAnimalFilters.length; counter++){
+    //console.log(counter);
+    let currentFilter = arrAnimalFilters[counter];
+    //console.log(currentFilter);
+    currentFilter.addEventListener('click',filterImages);
+}
+
+
+
+/*-----     Functions     ------*/
+function filterImages(event){
+    //console.log(event.target.innerHTML);
+    var animalName = event.target.innerHTML.toLowerCase();
+    console.log(animalName);
+    for(counter=0;counter < arrAnimalImages.length; counter++){
+        let currentImage = arrAnimalImages[counter];
+        currentImage.style.display= 'none';
+        console.log(currentImage);
+        if(animalName == 'all'){
+            currentImage.style.display = 'inline';
+        } else {
+            let currentImageAnimal = currentImage.getAttribute('animal');
+            //console.log(currentImageAnimal);
+            if(currentImageAnimal == animalName){
+                currentImage.style.display = 'inline';
+            }
+        }
+    }
+}
